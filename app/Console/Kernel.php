26e64e8e9fc058b79e40d45a34b9acdcaf93ec47
Weydans Barros <weydans@hotmail.com>
2023-08-14 12:01:58 -0300

    feat: create a job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\MyJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,6 +14,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function() {
+            MyJob::dispatch();
+        })->twiceDailyAt(6, 18, 10);
     }
 
     /**
